5e4ffdee9dce3f0dbf4dc716067fb5086b4327e1
Stephan Mauermann <stephan.mauermann@deepl.com>
2024-09-17 00:33:12 +0200

    fix: path

--- bootstrap/argocd.sh
+++ bootstrap/argocd.sh
@@ -7,4 +7,4 @@ kubectl kustomize --enable-helm ../kubernetes/infrastructure/workloads/argocd | 
 # we don't want that pesky % sign at the end of the password output
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
-kubectl apply -k kubernetes/meta
+kubectl apply -k ../kubernetes/meta
