9a250514b9398cefa5423477063c1a437c988599
Stephan Mauermann <stephan.mauermann@deepl.com>
2024-09-20 17:17:34 +0200

    test: op

--- bootstrap/onepassword.sh
+++ bootstrap/onepassword.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+#kubectl create namespace onepassword
+#kubectl create secret generic onepassword-credentials -n onepassword \
+#  --from-literal=credentials=your-credentials-here \
+#  --from-literal=token=your-token-here
+
 kubectl kustomize --enable-helm ../kubernetes/infrastructure/workloads/onepassword | kubectl apply -f -
