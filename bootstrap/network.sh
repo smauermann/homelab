41f9102b6750f5124c0be759ed97eee3c1765338
Stephan Mauermann <stephan.mauermann@deepl.com>
2024-09-22 17:58:55 +0200

    feat(bootstrap): configure ip forwarding

---
+++ bootstrap/network.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Enable IP forwarding
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+
+# Advertise routes of ciliums loadbalancer ip pool
+# Subnet routes must be approved in the tailscale admin console https://login.tailscale.com/admin/machines
+# --advertise-routes string: routes to advertise to other nodes (comma-separated, e.g. "10.0.0.0/8,192.168.0.0/24")
+sudo tailscale up --advertise-routes="192.168.178.195/32,192.168.178.196/32,192.168.178.197/32,192.168.178.198/32,192.168.178.199/32,192.168.178.200/32"
--- bootstrap/run-all.sh
+++ bootstrap/run-all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+./network.sh
 ./dependencies.sh
 ./k3s.sh
 ./cilium.sh
