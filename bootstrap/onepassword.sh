585ea6020e5c3dc10838b7729078f1d8008d91df
Stephan Mauermann <stephan.mauermann@deepl.com>
2024-09-22 12:20:39 +0200

    feat: I think we might have it working now

--- bootstrap/onepassword.sh
+++ bootstrap/onepassword.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+# setup onepassword credentials (connect to 1password auth) and token (operator to connect auth)
 kubectl create namespace onepassword
 cat 1password-credentials.json | base64 | tr -d \\n > credentials.b64
 kubectl -n onepassword create secret generic onepassword-credentials --from-file credentials=credentials.b64
-kubectl -n onepassword create secret generic onepassword-token --from-file token=token.txt
+rm credentials.b64
+kubectl -n onepassword create secret generic onepassword-token --from-env-file token.env
 
 kubectl kustomize --enable-helm ../kubernetes/infrastructure/workloads/onepassword | kubectl apply -f -
 
