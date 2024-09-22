c6aab408940ca1a2de21d0833d8f86650570d27d
Stephan Mauermann <stephan.mauermann@deepl.com>
2024-09-22 11:54:51 +0200

    fix: creds file name

--- bootstrap/onepassword.sh
+++ bootstrap/onepassword.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 kubectl create namespace onepassword
 cat 1password-credentials.json | base64 | tr -d \\n > credentials.b64
-kubectl -n onepassword create secret generic onepassword-credentials --from-file credentials=1password-credentials.b64
+kubectl -n onepassword create secret generic onepassword-credentials --from-file credentials=credentials.b64
 kubectl -n onepassword create secret generic onepassword-token --from-file token=token.txt
 
 kubectl kustomize --enable-helm ../kubernetes/infrastructure/workloads/onepassword | kubectl apply -f -
