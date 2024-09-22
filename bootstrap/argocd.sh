8c56c693051097dabc9c5ab3763de3fad668d28a
Stephan Mauermann <stephan.mauermann@deepl.com>
2024-09-22 12:25:39 +0200

    feat: better order

--- bootstrap/argocd.sh
+++ bootstrap/argocd.sh
@@ -4,7 +4,8 @@ set -ex
 
 kubectl kustomize --enable-helm ../kubernetes/infrastructure/workloads/argocd | kubectl apply -f -
 
+# create the meta apps (root apps that apply all other apps)
+kubectl apply -k ../kubernetes/meta
+
 # we don't want that pesky % sign at the end of the password output
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-
-kubectl apply -k ../kubernetes/meta
