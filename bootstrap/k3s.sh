82dd4d076a23f4fdc08a28398d93994494ab59e2
Stephan Mauermann <stephan.mauermann@deepl.com>
2024-10-10 22:38:13 +0200

    feat: k3s tweaks for prometheus

--- bootstrap/k3s.sh
+++ bootstrap/k3s.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-# install k3s with flannel disabled since we are going to use cilium
- curl -sfL https://get.k3s.io | sh -s - \
-   --write-kubeconfig-mode='0600' \
-   --flannel-backend=none \
-   --disable-kube-proxy \
-   --disable servicelb \
-   --disable-network-policy \
-   --disable traefik \
-   --disable-helm-controller \
-   --cluster-init
+# copy k3s config to the correct place
+cp config/k3s.yaml /etc/rancher/k3s/config.yaml
+
+# install k3s
+curl -sfL https://get.k3s.io | sh -s - --cluster-init
 
 # cp kubeconfig to the standard location
 mkdir -p $HOME/.kube
