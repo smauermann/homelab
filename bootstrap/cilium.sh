89edfc6ff0e1dc583890127c429983fd94e34033
Stephan Mauermann <stephan.mauermann@deepl.com>
2025-01-25 13:04:04 +0100

    chore: cleanup bootstrap folder

--- bootstrap/cilium.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# clean up old charts
-APP_DIR="../kubernetes/infrastructure/network/cilium"
-rm -rf $APP_DIR/charts
-
-kubectl kustomize --enable-helm $APP_DIR | kubectl apply -f -
-
-cilium status --wait
--- bootstrap/run-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-./dependencies.sh
-./network.sh
-./k3s.sh
-./cilium.sh
-./onepassword.sh
-./argocd.sh
